0decd0b74914665e67a3bc3fb94de4e629108649
baggasumit <baggasumit@gmail.com>
2018-03-10 01:52:58 -0800

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,27 @@
+const { mongoose } = require('../server/db/mongoose');
+const { Todo } = require('../server/models/todo');
+
+const id = '5aa39bb4e209999756534457';
+
+Todo.find({
+  _id: id,
+}).then((todos) => {
+  console.log('todos', todos);
+});
+
+Todo.findOne({
+  _id: id,
+}).then((todo) => {
+  console.log('todo', todo);
+});
+
+Todo.findById(id)
+  .then((todo) => {
+    if (!todo) {
+      return console.log('Unable to find todo');
+    }
+    console.log('todo by id', todo);
+  })
+  .catch((e) => {
+    console.log(e.name);
+  });
