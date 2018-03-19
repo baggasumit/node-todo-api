5f9de7856bc1741c9611a3b4048eae636b35c904
baggasumit <baggasumit@gmail.com>
2018-03-19 00:33:49 -0700

    Hash the user passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,20 +1,36 @@
-const { SHA256 } = require('crypto-js');
-const jwt = require('jsonwebtoken');
+// const { SHA256 } = require('crypto-js');
+// const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const message = 'I am user number 3';
-const hash = SHA256(message);
+const password = '123abc';
 
-console.log(hash);
-console.log(hash.toString());
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log('hash :: ', hash);
+  });
+});
 
-const data = {
-  id: 10,
-  name: 'Sumit',
-  isHome: false,
-};
+const hashedPassword =
+  '$2a$10$/9TVaOSWzIeCM1qyQI4eoefb35CvsotTss8swCMYN7724LmaBse6i';
 
-const token = jwt.sign(data, '123abc');
-console.log('token :: ', token);
+bcrypt.compare(password, hashedPassword, (err, result) => {
+  console.log('Result :: ', result);
+});
 
-const decoded = jwt.verify(token, '123abc');
-console.log('Decoded :: ', decoded);
+// const message = 'I am user number 3';
+// const hash = SHA256(message);
+
+// console.log(hash);
+// console.log(hash.toString());
+
+// const data = {
+//   id: 10,
+//   name: 'Sumit',
+//   isHome: false,
+// };
+
+// const token = jwt.sign(data, '123abc');
+// console.log('token :: ', token);
+
+// const decoded = jwt.verify(token, '123abc');
+// console.log('Decoded :: ', decoded);
