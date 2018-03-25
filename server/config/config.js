d4d4a1b84e69b86efa857fd61a92c909c6d66202
baggasumit <baggasumit@gmail.com>
2018-03-25 15:11:37 -0700

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,7 @@
 const env = process.env.NODE_ENV || 'development';
+const config = require('./config.json');
+const envConfig = config[env];
 
-switch (env) {
-  case 'development':
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-    break;
-  case 'test':
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-    break;
-  case 'production':
-    process.env.MONGODB_URI =
-      'mongodb://admin:admin@ds213199.mlab.com:13199/todo-app';
-}
+Object.keys(envConfig).forEach((key) => {
+  process.env[key] = envConfig[key];
+});
